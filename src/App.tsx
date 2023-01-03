fffe7f808df0f5bfea6499e77b64867bc18a1b8a
vadhee <rivaldi@zero-one-group.com>
2023-01-03 19:27:54 +0800

    feat: create componenr sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import { Text, Button, Stack } from "@mantine/core";
 import { ThemeProvider } from "./ThemeProvider";
-
+import {
+  Outlet,
+  RouterProvider,
+  createReactRouter,
+  createRouteConfig,
+  Link,
+  useMatch,
+  RegisteredAllRouteInfo,
+} from "@tanstack/react-router";
+import SideBar from "./components/SideBar";
+import { Grid, Skeleton, Container } from "@mantine/core";
 export default function App() {
+  const rootRoute = createRouteConfig();
+  const indexRoute = rootRoute.createRoute({
+    path: "/test",
+    component: () => <p>okkokok</p>,
+  });
+  const routeConfig = createRouteConfig().addChildren([
+    indexRoute,
+    // postsRoute.addChildren([PostsIndexRoute, postRoute]),
+  ]);
+  const router = createReactRouter({
+    routeConfig,
+    // defaultPreload: 'intent',
+  });
+  const child = <Skeleton height={140} radius="md" animate={false} />;
   return (
-    <ThemeProvider>
-      <Stack align="center" mt={50}>
-        <Text size="xl" weight={500}>
-          Welcome to Mantine!
-        </Text>
-        <Button>Click the button</Button>
-      </Stack>
-    </ThemeProvider>
+    <RouterProvider router={router}>
+      <ThemeProvider>
+        <Grid>
+          <Grid.Col span={4}>
+            <SideBar />
+          </Grid.Col>
+          <Grid.Col span={8}>
+            <p>okokokok</p>
+          </Grid.Col>
+        </Grid>
+      </ThemeProvider>
+      <Outlet />
+    </RouterProvider>
   );
 }
